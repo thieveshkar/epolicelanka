79ae9625f427a42a951b1ecc6705f71b623f2540
Thieveshkar <161707841+thieveshkar@users.noreply.github.com>
2025-07-26 00:27:56 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -7,26 +7,48 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+
+if (env === "production") {
+  // In production (Azure), build connection string from individual env vars
+  const {
+    DB_HOST,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_PORT,
+  } = process.env;
+
+  const connectionString = `postgresql://${DB_USER}:${encodeURIComponent(DB_PASSWORD)}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`;
+
+  sequelize = new Sequelize(connectionString, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else if (config.use_env_variable) {
+  // If config says to use env variable (usually development)
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  // Normal config with separate parameters
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js"
-    );
+  .filter((file) => {
+    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
   })
-  .forEach(file => {
+  .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
